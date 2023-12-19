b2178c6cad5a36c37161a6851b738d372bb5d3d7
NILOO <niloopour9@gmail.com>
2023-12-19 12:17:55 +0330

    create beans

--- src/main/java/ir/maktab/J2OS/util/ApplicationContext.java
+++ src/main/java/ir/maktab/J2OS/util/ApplicationContext.java
@@ -1,4 +1,32 @@
 package ir.maktab.J2OS.util;
 
+import ir.maktab.J2OS.repository.MediaCategoryRepository;
+import ir.maktab.J2OS.repository.impl.MediaCategoryRepositoryImpl;
+import ir.maktab.J2OS.service.MediaCategoryService;
+import ir.maktab.J2OS.service.impl.MediaCategoryServiceImpl;
+
+import javax.persistence.EntityManager;
+
 public class ApplicationContext {
+
+    private static final MediaCategoryRepository mediaCategoryRepository;
+    private static final MediaCategoryService mediaCategoryService;
+
+    static {
+        EntityManager entityManager = HibernateUtil.getTestEntityManagerFactory().createEntityManager();
+        mediaCategoryRepository = new MediaCategoryRepositoryImpl(entityManager);
+        mediaCategoryService = new MediaCategoryServiceImpl(mediaCategoryRepository);
+    }
+
+    private ApplicationContext() {
+
+    }
+
+    public static MediaCategoryRepository getMediaCategoryRepository() {
+        return mediaCategoryRepository;
+    }
+
+    public static MediaCategoryService getMediaCategoryService() {
+        return mediaCategoryService;
+    }
 }
